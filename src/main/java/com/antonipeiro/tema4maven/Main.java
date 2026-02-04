c5f8bd86dc0e3ec67427e5ee3029a3d7fa0fc645
Antoni Peiró Tarrazona <norussianxd@gmail.com>
2026-02-04 18:50:08 +0100

    Primer banner creado.

--- src/main/java/com/antonipeiro/tema4maven/Main.java
+++ src/main/java/com/antonipeiro/tema4maven/Main.java
@@ -1,6 +1,19 @@
 package com.antonipeiro.tema4maven;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String texto = "Antoni Peiró";
+
+        //creamos un try para asumir el riesgo de la excepción
+        try {
+            String banner = FigletFont.convertOneLine(texto);
+            System.out.println(banner);
+        } catch (IOException e) {
+            System.out.println("Error generating ASCII art");
+            e.printStackTrace();
         }
     }
+}
